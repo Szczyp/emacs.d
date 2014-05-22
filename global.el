d10a3926a015f61d300b104ad5da9017bf6933f3
Szczyp <qb@szczyp.com>
2014-05-22 11:50:17 +0200

    Add projectile and flx-ido

--- global.el
+++ global.el
@@ -1,6 +1,13 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
-(ido-mode)
+(package-require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
+
+(package-require 'projectile)
+(projectile-global-mode)
 
 (package-require 'ag)
 
