fa069b75de722da1df5f13206829776a0d003998
Szczyp <qb@szczyp.com>
2014-10-28 17:51:34 +0100

    Move all customizations to lisp subdir

--- init.el
+++ init.el
@@ -22,11 +22,7 @@
 ;; Add .emacs.d to load-path
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
-(add-to-list 'load-path dotfiles-dir)
-
-;; Set paths to custom.el and loaddefs.el
-(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq custom-file (concat dotfiles-dir "custom.el"))
+(add-to-list 'load-path (concat dotfiles-dir "lisp"))
 
 ;; Detect online status, from ESK
 (require 'cl)
